0551a62e2f0efe23031061ac95015f160cc4e1bf
Alan <2001alanlopez2001@gmail.com>
2023-11-22 21:06:57 -0300

    login+ registro listo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,18 +5,20 @@ import {
   Validators,
   FormBuilder
 } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-
 export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
 
-  constructor(public fb: FormBuilder) { 
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl:NavController) { 
 
     this.formularioLogin = this.fb.group({
       'nombre': new FormControl("",Validators.required),
@@ -26,6 +28,26 @@ export class LoginPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario')!);
+
+    if(usuario.nombre == f.nombre && usuario.password == f.password){
+      console.log('Ingresado');
+      localStorage.setItem('ingresando','true')
+      this.navCtrl.navigateRoot('/tabs/tab1')
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Datos incorrectos',
+        message: 'Los datos que ingresaste son incorrectos.',
+        buttons: ['Aceptar']
+      });
+  
+      await alert.present();
+    }
   }
 
 }
