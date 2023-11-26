e15a7b9855e3cd75fcfeb311f3787a2fa4c82003
Alan <2001alanlopez2001@gmail.com>
2023-11-26 00:49:45 -0300

    prueba de consumo nuevo servicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,58 @@ import { TaskService } from './services/task.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  title = 'angular-http-client';
   constructor(
     private taskService:TaskService
   ) {}
+
+
+  getAllTask(){
+    this.taskService.getAllTask()
+    .subscribe(tasks=> {
+      console.log(tasks);
+    })
+  }
+
+  getTask(){
+    this.taskService.getTask('1')
+    .subscribe(task=>{
+      console.log(task)
+    });
+  }
+
+  createTask() {
+    const task ={
+      id:'12',
+      userId :'1',
+      title:'charge title',
+      completed:true
+    };
+    this.taskService.createTask(task)
+    .subscribe((newTask)=> {
+      console.log(newTask);
+    });
+  }
+
+  updateTask() {
+    const task ={
+      id:'201',
+      userId :'1',
+      title:'otro titulo',
+      completed:true
+    };
+    this.taskService.updateTask(task)
+    .subscribe((newTask)=> {
+      console.log(newTask);
+    });
+  }
+
+  deleteTask(){
+    this.taskService.deleteTask('1')
+    .subscribe((data)=>{
+      console.log(data)
+    });
+  }
+
+
 }
